0ee895c7489808ecc34da33c026ee923b9bd9711
farbverlauf0 <93871580+farbverlauf0@users.noreply.github.com>
2022-02-16 21:33:11 +0300

    Update code.py

--- hw_2/code.py
+++ hw_2/code.py
@@ -1,11 +1,13 @@
 def latex_table(lst):
-    begin = r'\begin{tabular}{' + '| c ' * len(lst[0]) + r'|}\hline '
-    middle = ''.join(map(lambda row: ' & '.join(map(str, row)) + r'\\\hline ', lst))
-    return begin + middle + r'\end{tabular}'
-  
-  
+    begin = r'\begin{tabular}{' + '| c ' * len(lst[0]) + r'|}\hline'
+    middle = '\n'.join(map(lambda row: ' & '.join(map(str, row)) + r'\\ \hline ', lst))
+    return begin + '\n' + middle + '\n' + r'\end{tabular}'
+
+
 inp = [['cat', 12, 4], ['dog', 20, 8], ['human', 75, 32]]
 outp = latex_table(inp)
 
-with open('table.tex', 'w') as f:
+with open('table.tex', 'a') as f:
+    f.write('\\documentclass{article}\n\\usepackage[utf8]{inputenc}\\begin{document}\n')
     f.write(outp)
+    f.write('\n\\end{document}')
