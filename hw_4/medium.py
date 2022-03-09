343fc55daf353d1c241b72b83ee7804373ded4d6
farbverlauf0 <93871580+farbverlauf0@users.noreply.github.com>
2022-03-10 00:37:15 +0300

    Update medium.py

--- hw_4/medium.py
+++ hw_4/medium.py
@@ -11,21 +11,25 @@ def sync_integrate(f, a, b, n_iter):
         acc += f(a + i * step) * step
     return acc
 
-log = []
 def sync_integrate_with_log(args):
     f, a, b, n_iter, i, n_jobs = args
-    log.append(f'{i + 1}th job was executed at {datetime.now()} with n_jobs={n_jobs}\n')
+    sync_integrate_with_log.buffer += f'{i + 1}th job was executed at {datetime.now()} with n_jobs={n_jobs}\n'
     return sync_integrate(f, a, b, n_iter)
+
+sync_integrate_with_log.buffer = ''
 
 def integrate(f, a, b, *, n_jobs=1, n_iter=1000):
     step = (b - a) / n_jobs
     args = [(f, a + i * step, a + (i + 1) * step, n_iter // n_jobs, i, n_jobs) for i in range(n_jobs)]
     with ProcessPoolExecutor(max_workers=n_jobs) as executor:
-        res = sum(executor.map(sync_integrate_with_log, args))
-    return res
+        result = sum(executor.map(sync_integrate_with_log, args))
+    with open('medium_log.txt', 'a') as f:
+        f.write(sync_integrate_with_log.buffer)
+    sync_integrate_with_log.buffer = ''
+    return result
 
 if __name__ == '__main__':
-    cpu_count, n_iter = cpu_count(), 10 ** 8
+    cpu_count, n_iter = cpu_count(), 5 * 10 ** 8
 
     # Normal
     start = time.time()
@@ -39,11 +43,7 @@ if __name__ == '__main__':
     for n_jobs in range(1, 2 * cpu_count + 1):
         start = time.time()
         res = integrate(math.cos, 0, math.pi/2, n_jobs=n_jobs, n_iter=n_iter)
-        message = f'Processpoll with n_jobs={n_jobs} executed in {time.time() - start}, res={res}\n'
+        message = f'Processpool with n_jobs={n_jobs} executed in {time.time() - start}, res={res}\n'
         print(message, end='')
         with open('compare.txt', 'a') as f:
-            f.write(message)
-
-    with open('log.txt', 'a') as f:
-        for message in log:
             f.write(message)
