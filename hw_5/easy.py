02b06f592a88d1c8937a9c55c9680dc0a55102fb
farbverlauf0 <93871580+farbverlauf0@users.noreply.github.com>
2022-03-16 02:42:22 +0300

    Update easy.py

--- hw_5/easy.py
+++ hw_5/easy.py
@@ -4,32 +4,32 @@ import time
 
 
 def save_img(content, path):
-	with open(path, 'wb') as f:
-		f.write(content)
+    with open(path, 'wb') as f:
+        f.write(content)
 
 async def download_img(url, path, client):
-	async with client.get(url) as response:
-		save_img(await response.content.read(), path)
+    async with client.get(url) as response:
+        save_img(await response.content.read(), path)
 
 async def download_all_imgs(sites, path):
-  async with aiohttp.ClientSession() as client:
-		tasks = []
-		for i, url in enumerate(sites):
-			task = asyncio.create_task(download_img(url, path + 'img{}.jpg'.format(i), client))
-			tasks.append(task)
-		try:
-			await asyncio.gather(*tasks, return_exceptions=True)
-		except Exception as e:
-			print(repr(e))
+    async with aiohttp.ClientSession() as client:
+        tasks = []
+        for i, url in enumerate(sites):
+            task = asyncio.create_task(download_img(url, path + 'img{}.jpg'.format(i), client))
+            tasks.append(task)
+        try:
+            await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as e:
+            print(repr(e))
 
 async def main():
-	sites = ['https://picsum.photos/200'] * int(input())
-	path = 'C:/Users/dmvik/Desktop/imgs/'
-	start = time.time()
-	await download_all_imgs(sites, path)
-	print(f'Time: {time.time() - start}s')
+    sites = ['https://picsum.photos/200'] * int(input())
+    path = 'C:/Users/dmvik/Desktop/imgs/'
+    start = time.time()
+    await download_all_imgs(sites, path)
+    print(f'Time: {time.time() - start}s')
 
 
 if __name__ == '__main__':
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
